1194c82a08b385356154fa2044c999202aa85069
Pranita Dane <80359682+pranita28Dane@users.noreply.github.com>
2022-11-13 02:55:16 +0530

    Delete Course 6/W4 - Putting It All Together directory

--- Course 6/W4 - Putting It All Together/example_upload.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-
-# this example shows how a file can be uploaded using python requests module
-
-url = "http://localhost/upload/"
-with open("path","r") as opened:
-    r = requests.post(url, files={"files":opened})
